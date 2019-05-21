738fec6bbbe2b0fdc178c5c11c7849b3ef455952
perlancar (on pc-office) <perlancar@gmail.com>
2019-05-21 20:31:08 +0700

    Initial commit

---
+++ lib/App/LTSVUtils.pm
@@ -0,0 +1,31 @@
+package App::LTSVUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to LTSV
+
+=for Pod::Coverage ^(csvutil)$
+
+=head1 DESCRIPTION
+
+This distribution contains the following CLI utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 FAQ
+
+
+=head1 prepend:SEE ALSO
+
+L<https://ltsv.org>
+
+L<App::TSVUtils>
+
+L<App::CSVUtils>
+
+L<App::SerializeUtils>
+
+=cut
